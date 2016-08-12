7708562b011e4bc2279f78e2ae159f7ff7bea35b
Tom Macmichael <tommacmichael@eventbrite.com>
2016-08-12 13:42:41 -0700

    Fixed json response

--- at-line/urls.py
+++ at-line/urls.py
@@ -3,6 +3,7 @@ import api.views
 from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Examples:
@@ -11,10 +12,10 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^join/', api.views.JoinAPI.as_view(), name='join'),
-    url(r'^question/', api.views.QuestionAPI.as_view(), name='question'),
-    url(r'^session/', api.views.SessionAPI.as_view(), name='session'),
-    url(r'^create_player/', api.views.CreatePlayerAPI.as_view(), name='create_player'),
-    url(r'^results/', api.views.ResultsAPI.as_view(), name='result'),
-    url(r'^answer/', api.views.AnswerAPI.as_view(), name='answer'),
+    url(r'^join/', csrf_exempt(api.views.JoinAPI.as_view()), name='join'),
+    url(r'^question/', csrf_exempt(api.views.QuestionAPI.as_view()), name='question'),
+    url(r'^session/', csrf_exempt(api.views.SessionAPI.as_view()), name='session'),
+    url(r'^create_player/', csrf_exempt(api.views.CreatePlayerAPI.as_view()), name='create_player'),
+    url(r'^results/', csrf_exempt(api.views.ResultsAPI.as_view()), name='result'),
+    url(r'^answer/', csrf_exempt(api.views.AnswerAPI.as_view()), name='answer'),
 ]

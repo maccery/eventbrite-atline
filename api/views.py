fc0154f59fc72575333011a6aa0886598c75619e
Tom Macmichael <tommacmichael@eventbrite.com>
2016-08-11 22:07:05 -0700

    Added test suite with factories

--- api/views.py
+++ api/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.core import serializers
-from api.models import Session, Game, Question
+from api.models import Session, Game, Player, Question
 from django.db.models import Count
 
 
-class JoinAPI(object):
+class API(object):
 
     def join(self, request):
         if request.method == 'POST':
             game_id = request.POST.get('game_id')
 
             # check if the game id is valid
-            self._check_game_id_valid(game_id)
+            game = self._check_game_id_valid(game_id)
+
             # Find available sessions
-            session = self._get_or_create_session(game_id)
+            session = self._get_or_create_session(game)
             # Get 6 random questions and add it to the session
             self._assign_questions_to_session(session)
 
-            return serializers.serialize('json', session)
+            return serializers.serialize('json', [session,])
         else:
             self._throw_api_error('We need a GET request')
 
@@ -27,25 +28,25 @@ class JoinAPI(object):
 
         if not game:
             self._throw_api_error('No game with this ID')
+        else:
+            return game[0]
 
     def _assign_questions_to_session(self, session):
         questions = Question.objects.all().order_by('?')[:6]
-        print dir(session)
-        session.related_set.set(questions)
+        session.questions.set(questions)
 
-    def _get_or_create_session(self, game_id):
+    def _get_or_create_session(self, game):
         available_session = Session.objects.annotate(players_count=Count('players')).filter(players_count__lte=7).count()
 
         if available_session:
-            session = available_session
+            session = Session.objects.filter(pk=available_session)[0]
 
         else:
-            session = Session.objects.create(game_id=game_id)
-            session.save()
+            session = Session.objects.create(game=game)
 
         return session
 
     def _throw_api_error(self, message):
         message = [message]
 
-        return message
+        raise Exception('ERROR')

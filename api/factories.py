5dc15843a90cbffb396e5283846d27238c3282d0
Tom Macmichael <tommacmichael@eventbrite.com>
2016-08-11 22:49:43 -0700

    Fixed merge conflicts

--- api/factories.py
+++ api/factories.py
@@ -1,18 +1,20 @@
 from api.models import Session, Game, Question, Player
 
+
 class QuestionFactory():
+
     def create(self, number_of_questions):
         for i in range(0, number_of_questions):
             Question.objects.create(
                 text='hey',
-                answer=1,
                 first_option='hey',
                 second_option='hey',
                 third_option='hey',
-                fourth_option='hey',
             )
 
+
 class PlayerFactory():
+
     def create(self, session, number_of_players):
         for i in range(0, number_of_players):
             player = Player.objects.create()

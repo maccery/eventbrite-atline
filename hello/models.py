63af5e5f8687c922f51ff2e00916c610dcdff772
eyang <eyang-eb@eventbrite.com>
2016-08-11 17:28:12 -0700

    Added models and relationships

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+class Session(models.Model):
+    status = models.CharField(max_length=30)
+    game_id = models.IntegerField
+    question = models.ManyToManyField(Question)
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=90)
+    answer = models.IntegerField(max_length=1)
+    first_option = models.CharField(max_length=15)
+    second_option = models.CharField(max_length=15)
+    third_option = models.CharField(max_length=15)
+    fourth_option = models.CharField(max_length=15)
+    first_count = models.IntegerField
+    second_count = models.IntegerField
+    third_count = models.IntegerField
+    fourth_count = models.IntegerField
+
+
+class Game(models.Model):
+    event_id = models.IntegerField
+
+
+class Prize(models.Model):
+    game_id = models.IntegerField
+    inventory_count = models.IntegerField
+
+
+class Player(models.Model):
+    ticket_id = models.IntegerField
+    prize = models.ManyToManyField(Prize)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
